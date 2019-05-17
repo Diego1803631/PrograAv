b6efe84e77a63c52cd74e3719ad4574accfab76e
Diego Flores <42399052+Diego1803631@users.noreply.github.com>
2019-05-17 00:19:23 -0500

    Act

---
+++ resource2.h
@@ -0,0 +1,64 @@
+//{{NO_DEPENDENCIES}}
+// Archivo de inclusión generado de Microsoft Visual C++.
+// Usado por Agenda.rc
+//
+#define IDOK2                           2
+#define IDCRESET                        3
+#define IDD_DIALOG1                     101
+#define IDR_MENU1                       102
+#define IDD_DIALOG2                     104
+#define IDD_DIALOG3                     106
+#define IDR_MENU2                       108
+#define IDD_DIALOG4                     109
+#define IDD_DIALOG5                     111
+#define IDD_DIALOG6                     113
+#define IDR_MENU3                       115
+#define IDC_NOMBRE                      1002
+#define IDC_TELEFONO                    1003
+#define IDC_ESPECIE                     1004
+#define IDC_MASCOTA                     1005
+#define IDC_MOTIVO                      1006
+#define IDC_COSTO                       1007
+#define IDC_APELLIDO                    1008
+#define IDC_FECHA                       1010
+#define IDC_HORA                        1011
+#define IDC_IMAGE                       1014
+#define IDC_LISTA                       1023
+#define IDC_DOC                         1024
+#define IDC_CED                         1026
+#define IDC_INF                         1027
+#define IDC_BUTTON1                     1040
+#define IDC_EDIT1                       1041
+#define IDC_MASCULINO                   1044
+#define IDC_FEMENINO                    1045
+#define IDC_URGENTE                     1046
+#define IDC_GENERO                      1047
+#define IDC_CHECK1                      1053
+#define IDC_CANCEL                      1056
+#define ID_ARCHIVO_GUARDARYSALIR        40001
+#define ID_NUEVO_CITA                   40002
+#define ID_INFORMACI40003               40003
+#define ID_AGENDA_CITASAGENDADAS        40004
+#define ID_AGENDA_EDITARCITA            40005
+#define ID_NUEVA_CITA                   40006
+#define ID_ARCHIVO_SALIR                40007
+#define ID_INFORMAD                     40008
+#define ID_EDITAR_DATOSDELDOCTOR        40009
+#define ID_EDITAR_CITA                  40010
+#define ID_VER_AGENDA                   40011
+#define ID_AGENDA_CITASAGREGADAS        40012
+#define ID_ARCHIVO_EDITAR               40013
+#define ID_ARCHIVO_ELIMINAR             40014
+#define ID_ARCHIVO_EDITAR40015          40015
+#define ID_ARCHIVO_EDITD                40016
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        118
+#define _APS_NEXT_COMMAND_VALUE         40017
+#define _APS_NEXT_CONTROL_VALUE         1060
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
